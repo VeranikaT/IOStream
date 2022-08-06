f0f39992e2b9202869037ef45c2f59a9ee1449e5
Veranika Trafimchuk <veranika_trafimchuk@epam.com>
2022-08-06 21:00:09 +0400

    new code

--- src/com/company/java/iostream/Main.java
+++
@@ -1,8 +0,0 @@
-package com.company.java.iostream;
-
-public class Main {
-
-    public static void main(String[] args) {
-        IOStream.ioKeyWords("C:\\JavaBasic\\stream\\src\\com\\company\\java\\iostream\\FileToRead.java");
-    }
-}
